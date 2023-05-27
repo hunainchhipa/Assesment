14a94bd146c4fd07bca1dc25b89f352a87aa0a40
hunainchhipa <hunain.chhipa23@gmail.com>
2023-05-27 14:47:32 +0530

    Assesment

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+// import App from './App';
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap5/src/css/bootstrap.min.css";
+import Search from "./Compo/Search";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Search />
   </React.StrictMode>
 );
 
